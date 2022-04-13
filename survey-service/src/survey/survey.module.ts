2976c57a6d4cd9476c2e47ab98e462faefa37f18
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-13 11:29:51 -0600

    📅  | Wed Apr 13 11:29:51 MDT 2022

--- survey-service/src/survey/survey.module.ts
+++ survey-service/src/survey/survey.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SurveyService } from './survey.service';
 import { SurveyController } from './survey.controller';
+import { TypeORMError } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Response } from './entities/response.entity';
+import { Survey } from './entities/survey.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Survey, Response])],
   controllers: [SurveyController],
-  providers: [SurveyService]
+  providers: [SurveyService],
 })
 export class SurveyModule {}
