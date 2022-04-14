4d9f76c9e75e701c1c94a153da5b9de63c7b8645
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-14 12:03:16 -0600

    📅  | Thu Apr 14 12:03:16 MDT 2022

--- client/src/app/create-survey/create-survey.module.ts
+++ client/src/app/create-survey/create-survey.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CreateSurveyComponent } from './create-survey.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CreateSurveyService } from './create-survey.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [CreateSurveyComponent],
   providers: [CreateSurveyService],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   exports: [CreateSurveyComponent],
 })
 export class CreateSurveyModule {}
--- client/src/app/create-survey/create-survey.service.ts
+++ client/src/app/create-survey/create-survey.service.ts
@@ -1,14 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
+import { ConstantsService } from '../services/constants.service';
 
+type CreateSurveyDto = {
+  authorId: number;
+  name: string;
+  answers: string[];
+};
 @Injectable({
   providedIn: 'root',
 })
 export class CreateSurveyService {
-  constructor() {}
-
+  constructor(
+    private readonly constants: ConstantsService,
+    private http: HttpClient
+  ) {}
   addSurvey() {
-    console.log('hello');
-    //return this.http.post<Survey>();
+    console.log(this.constants.surveyUrl);
+    return this.http
+      .post(this.constants.surveyUrl, { foo: 'foo' })
+      .subscribe((value) => console.log(value));
   }
 }
