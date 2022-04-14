c145dabd1fea630f1f776a6b016b816a05a87ff8
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-14 10:42:54 -0600

    📅  | Thu Apr 14 10:42:54 MDT 2022

--- client/src/app/create-survey/create-survey.component.ts
+++ client/src/app/create-survey/create-survey.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-create-survey',
@@ -10,7 +10,43 @@ export class CreateSurveyComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
   createSurveyForm = this.fb.group({
     surveyName: [''],
+    question: [''],
+    answers: this.fb.array([this.fb.control('')]),
   });
+  answerLetters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+  get answers() {
+    return this.createSurveyForm.get('answers') as FormArray;
+  }
+  addAnswer() {
+    this.answers.push(this.fb.control(''));
+  }
 
   ngOnInit(): void {}
 }
