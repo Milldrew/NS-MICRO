6a8b2fcd640379ea92c746d2c384bf19afc14aae
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-15 10:27:34 -0600

    📅  | Fri Apr 15 10:27:34 MDT 2022

--- survey-service/src/response/entities/response.entity.ts
+++ survey-service/src/response/entities/response.entity.ts
@@ -1,1 +1,13 @@
-export class Response {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Response {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  authorId: number;
+  @Column()
+  surveyId: number;
+  @Column()
+  answerIndex: number;
+}
