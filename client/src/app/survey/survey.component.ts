11f21a1e341c13b9f7ed2eb248182de7fa134d04
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-14 20:59:09 -0600

    📅  | Thu Apr 14 20:59:09 MDT 2022

--- client/src/app/survey/survey.component.ts
+++ client/src/app/survey/survey.component.ts
@@ -6,6 +6,34 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./survey.component.css'],
 })
 export class SurveyComponent implements OnInit {
+  questionId = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
   constructor() {}
 
   @Input()
