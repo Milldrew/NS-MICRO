9128917c9f5ccbd19db5f995937915d1aab11eea
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-16 15:49:30 -0600

    📅  | Sat Apr 16 15:49:30 MDT 2022

--- client/src/app/services/constants.service.ts
+++ client/src/app/services/constants.service.ts
@@ -9,4 +9,8 @@ export class ConstantsService {
   myResponsesUrl = `http://192.168.0.143:3000/response/my`;
   mySurveysUrl = `http://192.168.0.143:3000/survey/my`;
   newsFeedUri = `ws://192.168.0.143:3002`;
+
+  newsFeedPaginationUrl(offset: number, limit: number) {
+    return `http://192.168.0.143:3000/survey?offset=${offset}&limit=${limit}`;
+  }
 }
