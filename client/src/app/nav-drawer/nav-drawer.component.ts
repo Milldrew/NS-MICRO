bafd6a19000abaa0b44f604643131a10384de293
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-18 21:00:07 -0600

    📅  | Mon Apr 18 21:00:07 MDT 2022

--- client/src/app/nav-drawer/nav-drawer.component.ts
+++ client/src/app/nav-drawer/nav-drawer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-drawer.component.css'],
 })
 export class NavDrawerComponent implements OnInit {
-  isOpen: boolean = false;
+  isOpen: boolean = true;
   menuItems = [
     {
       name: 'My Surveys',
