525a9e440e55f6b9e47c8546b09ec0f56790cef8
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-25 13:37:46 -0600

    📅  | Mon Apr 25 13:37:46 MDT 2022

--- client/src/app/sign-in/sign-in.component.ts
+++ client/src/app/sign-in/sign-in.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  signInForm = this.fb.group({ username: [''], password: [''] });
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  signIn() {
+    alert(JSON.stringify(this.signInForm.value));
   }
-
 }
