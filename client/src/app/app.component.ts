40a48f344a061a93016636ef6e24bcff0c2bc709
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-25 12:55:29 -0600

    📅  | Mon Apr 25 12:55:29 MDT 2022

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { NewsFeedStoreService } from './news-feed/services/news-feed-store.service';
+import { SurveyFeedService } from './news-feed/services/survey-feed.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'client';
+  constructor(
+    private surveyFeed: SurveyFeedService,
+    public newsStore: NewsFeedStoreService
+  ) {}
+
+  ngOnInit() {
+    this.surveyFeed.listen('newsFeed').subscribe((data) => {
+      this.newsStore.addNews(data);
+    });
+  }
 }
