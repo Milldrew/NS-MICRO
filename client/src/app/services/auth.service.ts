08c6db15a09a4e2585a8f75086a364839434df8a
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-26 11:42:35 -0600

    📅  | Tue Apr 26 11:42:35 MDT 2022

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,16 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ConstantsService } from './constants.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private constants: ConstantsService) {}
   signIn() {
     console.log('signin service');
   }
 
   register() {
     console.log('regiser service');
+    this.http
+      .post(this.constants.register, {
+        username: 'foobar4000',
+        password: 'foobar4000',
+      })
+      .subscribe(
+        (value) => console.log(value),
+        (warn) => console.warn(warn)
+      );
   }
 }
